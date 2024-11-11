ec535d1d5c7d120a58b16bace66250f4db5232cb
Danilo Nicolás Lorefice <dnlegithub@gmail.com>
2024-11-11 17:48:25 -0300

    Primer commit libreria

---
+++ ConsoleApp3/Libros.cs
@@ -0,0 +1,129 @@
+﻿using Bibliotecaa;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bibliotecaa
+{
+    public class Libro
+
+    {
+        public string Autor { get; set; }
+        public string Nombre { get; set; }
+        public int ISBN { get; set; }
+        public bool Disponible { get; set; }
+
+        public Libro(string Nombre, string Autor, int ISBN)
+        {
+
+            this.Autor = Autor;
+            this.Nombre = Nombre;
+            this.ISBN = ISBN;
+            this.Disponible = true;
+
+        }
+
+        
+
+        
+    }
+        
+
+
+    public class Usuario {
+
+        public int Id { get; set; }
+
+        public string Nombre { set; get; }
+
+        public string[] ListaDeLibros { set; get; }
+
+        public Usuario(int Id, string Nombre){
+            
+            this.Id = Id;
+            this.Nombre = Nombre;
+            this.ListaDeLibros = new string[99];
+            
+        }
+        public void agregarLibroAUsuario(Libro libro) {
+            bool var = true;
+            int i = 0;
+            while (var == true) {
+         
+                if (this.ListaDeLibros[i] == null ) {
+                    this.ListaDeLibros[i] = libro.Nombre;
+                    var = false;
+                }
+                i++;
+                
+            }
+        }
+
+    }
+
+
+    public class Biblioteca
+    {
+        List<Libro> libreria = new List<Libro>();
+        List<Usuario> listaUsuarios = new List<Usuario>();
+
+        public void agregarLibros(Libro libro){
+            libreria.Add(libro);
+        }
+        public void agregarUsuarios(Usuario usuario) { 
+            listaUsuarios.Add(usuario);
+        }
+        public string mostrarLibros() {
+            string dat = ""; 
+            foreach (Libro libro in libreria) {
+                
+                dat += $"-Titulo: {libro.Nombre}, Autor: {libro.Autor}, ISBN: {libro.ISBN}, Disponible: {libro.Disponible}" + "\n";
+                                
+            }
+
+            return dat;
+        }
+        public bool prestarLibro(int ISBM, int id) {
+            foreach (Libro libro in libreria) {
+                if (libro.ISBN == ISBM && libro.Disponible == true) {
+                    libro.Disponible = false;
+                    foreach (Usuario usuario in listaUsuarios) {
+                        if (usuario.Id == id) {
+                            usuario.agregarLibroAUsuario(libro);
+                        }
+                    }
+                    return true;
+                }
+            }
+            return false;
+        }
+        public bool devolverLibro(int ISBN) {
+            foreach (Libro libro in libreria) {
+                if(libro.ISBN == ISBN) {
+                    libro.Disponible = true;  
+                    return true;
+                }
+            }
+            return false;
+        }
+        public string mostrarLibrosPrestadosAUsuario(int ID) {
+            string var = "";
+            foreach (Usuario usuario in listaUsuarios) {
+                if (usuario.Id == ID) {
+                    foreach (var libro in usuario.ListaDeLibros) {
+                        if (libro != null) {
+                            var += libro + "\n"; 
+                        }
+                    } 
+                }return var;
+            }
+            return "El usuario no tiene ningun libro en su historial";
+        }
+
+    }
+    }
+
+
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,102 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Bibliotecaa;
+
+Biblioteca biblioteca = new Biblioteca();
+int accion = 0;
+while (accion != 7) {
+    string strVar1 = "";
+    string strVar2 = "";
+    int intVar1 = 0;
+    int intVar2 = 0;
+    bool response = false;
+    
+
+    Console.WriteLine("Bienvenido a la biblioteca \n 1-Agregar un libro \n 2-Registrar un usuario \n 3-Prestar un libro \n 4-Devolver un libro \n 5-Ver el estado de todos los libros" +
+    "\n 6-Ver libros prestados a un usuario \n 7-Salir \n Seleccionar una opcion ");
+
+    accion = int.Parse(Console.ReadLine());
+    switch (accion) {
+        case 1:
+            {    Console.WriteLine("Ingrese el título del libro: ");
+                 strVar1 = Console.ReadLine();
+                 Console.WriteLine("Ingrese el autor del libro");
+                 strVar2 = Console.ReadLine();
+                 Console.WriteLine("Ingrese el ISBN del libro");
+                 intVar1 = int.Parse(Console.ReadLine());
+                 Libro libro = new Libro(strVar1, strVar2, intVar1);
+                 biblioteca.agregarLibros(libro);
+                 Console.WriteLine("Libro agregado con éxito.");
+                 break;
+            }
+
+        case 2:
+            {
+                Console.WriteLine("Ingrese el nombre de usuario: ");
+                strVar1 = Console.ReadLine();
+                Console.WriteLine("Ingrese el ID del usuario: ");
+                intVar1 = int.Parse(Console.ReadLine());
+                Usuario usuario = new Usuario(intVar1, strVar1);
+                biblioteca.agregarUsuarios(usuario);
+                Console.WriteLine("Usuario registrado con éxito.");
+                break;
+            }
+        case 3:
+            {
+                Console.WriteLine("Ingrese el ISBN del libro a prestar: ");
+                intVar1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese el Id del usuario: ");
+                intVar2 = int.Parse(Console.ReadLine());
+                response = biblioteca.prestarLibro(intVar1, intVar2);
+                if (response == true)
+                {
+                    Console.WriteLine("Libro prestado con éxito");
+                }
+                else
+                {
+                    Console.WriteLine("Error al prestar libro");
+                }
+                break;
+            }
+        case 4:
+            {
+                Console.WriteLine("Ingrese el ISBN a devolver: ");
+                intVar1 = int.Parse(Console.ReadLine());
+                response = biblioteca.devolverLibro(intVar1);
+                if (response == true)
+                {
+                    Console.WriteLine("Libro devuelto con éxito.");
+                }
+                else {
+                    Console.WriteLine("Ocurrio un error");
+                }
+                break;
+            }
+        case 5:
+            {
+                Console.WriteLine("Estado de los libros: \n"+ biblioteca.mostrarLibros());
+                biblioteca.mostrarLibros();
+                break;
+            }
+        case 6: 
+            {
+                Console.WriteLine("Ingrese el Id del usuario: ");
+                intVar1 = int.Parse(Console.ReadLine());
+                Console.WriteLine(biblioteca.mostrarLibrosPrestadosAUsuario(intVar1));
+                break;
+            }
+        case 7:
+            {
+                Console.WriteLine("Saliendo de la aplicacion.");
+                break;
+            }
+        default: 
+            {
+                Console.WriteLine("opción errónea");
+                break;
+            }
+    }
+        
+
+}
+
+
